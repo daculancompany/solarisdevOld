f4e511701a93bb1836e1f0ecc817ef8a0740a80b
Niel Daculan <logicbase.daculan@gmail.com>
2020-02-05 13:29:36 +0800

    chnages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 var bodyParser = require('body-parser');
 
